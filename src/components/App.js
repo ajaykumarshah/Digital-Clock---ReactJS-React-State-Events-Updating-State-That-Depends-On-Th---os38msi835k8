c39088e10246aa18ff09c3483da1deed1b91fed2
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-07 22:54:25 +0530

    m

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 const App = () => {
+  let time = new Date();
+  time = time.toLocaleString();
+  // console.log(time);
+  const [currenttime, setTime] = useState(time);
+
+  useEffect(() => {
+    let id = setInterval(() => {
+      time = new Date();
+      time = time.toLocaleString();
+      setTime(time);
+
+    })
+
+    return () => {
+      // console.log(time);
+      clearInterval(id);
+    }
+  }, [currenttime])
+
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+        {currenttime}
+
+      </div>
     </div>
   )
 }
