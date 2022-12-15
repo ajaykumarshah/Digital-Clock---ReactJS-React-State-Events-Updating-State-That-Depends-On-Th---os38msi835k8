238d9d7a7ecc685d9baaf715b726a77831a0a449
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-12-15 14:21:27 +0530

    g

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/App.css';
-const App = () => {
-  let time = new Date();
-  time = time.toLocaleString();
-  // console.log(time);
-  const [currenttime, setTime] = useState(time);
+import React, { Fragment } from 'react'
+// import '../styles/App.css';
+// const App = () => {
 
-  useEffect(() => {
-    let id = setInterval(() => {
-      time = new Date();
-      time = time.toLocaleString();
-      setTime(time);
-
-    })
-
-    return () => {
-      // console.log(time);
-      clearInterval(id);
-    }
-  }, [currenttime])
+//   return (
+//     <div id="main">
+//       <div className="date-time"></div>
+//     </div>
+//   )
+// }
 
 
-  return (
-    <div id="main">
-      <div className="date-time">
-        {currenttime}
+// export default App;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <div className="date-time">
+          <h2>It is {this.state.date.toLocaleString()}.</h2>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
-
-
 export default App;
